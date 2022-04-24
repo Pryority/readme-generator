93b3410cc8213578ff1a1d965cfb3c278a907a69
Matthew Pryor <matthewapryor@gmail.com>
2022-04-24 18:27:28 -0400

    add template literal content for badge

--- Develop/index.js
+++ Develop/index.js
@@ -2,9 +2,7 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const util = require('util');
-const generatePage = require('./src/generate-readme.js');
-const generateReadme = require('../src/generate-readme.js');
-
+const generateMarkdown = require("./Develop/utils/generateMarkdown")
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -25,76 +23,55 @@ const questions = [
         type: 'confirm',
         name: 'confirmInstallationInstructions',
         message: 'Explain how the project should be installed.',
-        default: true
     },
     // USAGE
     {
         type: 'confirm',
         name: 'confirmUsage',
-        message: 'Would you like to give the project a section that explains how it should be used?',
-        default: true
+        message: 'Explain how the project should be used.',
     },
     {
         type: 'input',
         name: 'usage',
-        message: "Provide instructions and examples for use. Include screenshots as needed.",
-        when: ({ confirmUsage }) => confirmUsage
+        message: "Provide instructions and examples for use.",
     },
     // LICENSE
     {
-        type: 'confirm',
-        name: 'confirmLicense',
-        message: 'Would you like to give the project a section that displays the license used?',
-        default: true
-    },
-    {
         type: 'input',
         name: 'license',
-        message: "Enter the license used for this project to let other developers know what they can and cannot do with your project. If you need help choosing a license, refer to https://choosealicense.com.",
-        when: ({ confirmLicense }) => confirmLicense
+        message: 'What license will the project be using?',
     },
     // CONTRIBUTING
-    {
-        type: 'confirm',
-        name: 'confirmContributing',
-        message: 'Would you like to give the project a section that tells other developers that the project is open for contributions?',
-        default: true
-    },
     {
         type: 'input',
         name: 'contributions',
         message: "Include guidelines for how other developers can contribute to this project.",
-        when: ({ confirmContributing }) => confirmContributing
     },
     // CREDITS
-    {
-        type: 'confirm',
-        name: 'confirmCredits',
-        message: 'Would you like to give the project a section that gives credit to contributing developers, third-party assets or tutorials?',
-        default: true
-    },
     {
         type: 'input',
         name: 'credits',
         message: "List your collaborators, if any, with links to their GitHub profiles. If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section. If you followed tutorials, include links to those here as well.",
-        when: ({ confirmCredits }) => confirmCredits
     },
     // TESTS
-    {
-        type: 'confirm',
-        name: 'confirmTests',
-        message: 'Would you like to give the project a section that gives credit to contributing developers, third-party assets or tutorials?',
-        default: true
-    },
     {
         type: 'input',
         name: 'tests',
         message: "Provide examples of how to run tests for the project.",
-        when: ({ confirmTests }) => confirmTests
     }];
 
+promptInput = questions => {
+    return inquirer.prompt(questions);
+}
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) { }
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, $template, err => {
+        if (err) {
+            console.log(err);
+        }
+        console.log('Your readme file has been generated!')
+    });
+}
 
 // TODO: Create a function to initialize app
 function init() { }
@@ -103,7 +80,5 @@ function init() { }
 init()
     .then(promptInput)
     .then(readmeData => {
-        const readmeContent = generateReadme(readmeData);
-    })
-
-    ;
+        const readmeContent = generateMarkdown(readmeData);
+    });
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,65 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(response) {
+  if (response.licence === "MIT") {
+    badge = `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)\n\n
+    (https://opensource.org/licenses/MIT)\n\nYou have the freedom to do as you like with this permissive software, as long as an original copy and license notice is included. I cannon be held liable for this software.\n\n`;
+    return badge
+  } else if (response.licence === "Apache") {
+    badge = `[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)\n\n
+    (http://www.apache.org/licenses/LICENSE-2.0.html)\n\nYou have the freedom to do as you like with this permissive software. This license also contains a patent license from the contributors of the code.\n\n`;
+    return badge
+  } else if (response.licence === "GPLv3") {
+    badge = `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)\n\n
+    (http://www.gnu.org/licenses/gpl-3.0.html)\n\nYou have the freedom to run, study, share, and modify this permissive software. Anyone who acquires this software must make it available to anyone else under the same licensing agreement.\n\n`;
+    return badge
+  };
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) { }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) { }
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
+function generateMarkdown(answers) {
+  return `
+    <h1 align="center">${answers.projectTitle} 👋</h1>
+      
+    ![badge](${badge})<br />
+    ## Description
+    🔍 ${answers.description}
+    ## Table of Contents
+    - [Description](#description)
+    - [Installation](#installation)
+    - [Usage](#usage)
+    - [License](#license)
+    - [Contributing](#contributing)
+    - [Tests](#tests)
+    - [Questions](#questions)
+    ## Installation
+    💾 ${answers.installation}
+    ## Usage
+    💻 ${answers.usage}
+    ## License
+    ![badge](https://img.shields.io/badge/license-${answers.license}-brightgreen)
+    <br />
+    This application is covered by the ${answers.license} license. 
+    ## Contributing
+    👪 ${answers.contributing}
+    ## Tests
+    ✏️ ${answers.tests}
+    ## Questions
+    ✋ ${answers.questions}<br />
+    <br />
+    :octocat: Find me on GitHub: [${answers.username}](https://github.com/${answers.username})<br />
+    <br />
+    ✉️ Email me with any questions: ${answers.email}<br /><br />
+    _This README was generated with ❤️ by [README-generator](https://github.com/jpd61/README-generator) 🔥🔥🔥_
+  `;
 }
 
 module.exports = generateMarkdown;
